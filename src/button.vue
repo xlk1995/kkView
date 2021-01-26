7faff58fcf0a2812e3f9df7e45181491b513ce9b
xlk <1148038404@qq.com>
2021-01-26 16:14:24 +0800

    添加button的icon和loading

--- src/button.vue
+++ src/button.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>
-    <button class="kk-button">按钮</button>
-  </div>
+
+  <button class="kk-button">
+    <svg class="i-font" :class="{[`k-${position}`]: true, ['k-loadding']: type === 'loading'}" v-if="type">
+      <use :xlink:href="`#i-${type}`">
+      </use>
+    </svg>
+    <div :class="{[`k-${position==='left' ? 'right': 'left'}`]: true}">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
 export default {
+  props: {
+    type: {
+
+    },
+    position: {
+      type: String,
+      default: 'left',
+      validator: (val) => {
+        console.log(val);
+        return val === 'left' || val === 'right'
+
+      }
+    }
+  },
   data () {
     return {
 
@@ -24,6 +45,16 @@ export default {
 </script>
 
 <style scoped lang='scss'>
+@keyframes spin {
+  0%{
+    transform: rotate(0deg)
+  }
+  100%{
+    transform: rotate(360deg)
+  }
+  
+
+}
 .kk-button {
   height: var(--button-height);
   padding: 0 1em;
@@ -32,6 +63,20 @@ export default {
   background: var(--button-bg);
   border-radius: var(--button-border-radius);
   border: 1px solid var(--button-border-color);
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
+  .k-left {
+    order: 1;
+    margin-right: 0.4em;
+  }
+  .k-right {
+    order: 2;
+  }
+  .k-loadding{
+    animation: spin 1s linear infinite;
+  }
   &:hover {
     border-color: var(--button-hover-color);
   }
