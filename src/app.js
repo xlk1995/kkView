b63b263b2192b158c453997c1533e5b05f03960f
xlk <1148038404@qq.com>
2021-01-26 17:39:47 +0800

    创建button-group组件 解决边框问题

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('kk-button', Button)
 Vue.component('kk-icon', Icon)
+Vue.component('kk-button-group', ButtonGroup)
 new Vue({
   el: '#app',
   data:{
---
+++ src/button-group.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="k-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+   return {
+
+    };
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style scoped lang='scss'>
+.k-button-group{
+  display: inline-flex;
+  vertical-align: middle;
+  >.kk-button{
+    border-radius: 0;
+    margin-left: -1px;
+    &:first-child{
+      border-top-left-radius: var(--button-border-radius);
+      border-bottom-left-radius: var(--button-border-radius);
+    }
+    &:last-child{
+      border-top-right-radius: var(--button-border-radius);
+      border-bottom-right-radius: var(--button-border-radius);
+      
+    }
+    &:hover{
+      z-index: 1;
+    }
+  }
+  
+}
+</style>
