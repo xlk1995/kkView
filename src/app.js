47e6041b4ff509ee62d79c95252ef0b5dd092093
xlk <1148038404@qq.com>
2021-01-27 16:04:07 +0800

    四个单元测试

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import chai from 'chai'
+
+var expect = chai.expect
 
 Vue.component('kk-button', Button)
 Vue.component('kk-icon', Icon)
@@ -16,3 +19,70 @@ new Vue({
     
   }
 })
+
+// 测试props
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      type: 'left'
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')  
+  expect(href).to.eq('#i-left')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  let vm = new Constructor({
+    propsData:{
+      type: 'left',
+      loading: true
+    }
+  })
+  vm.$mount(div)
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  
+  expect(href).to.eq('#i-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  let div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  let vm = new Constructor({
+    propsData:{
+      type: 'setting',
+      position: 'left'
+    },
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.equal('1')
+}
+
+{
+  let div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  let vm = new Constructor({
+    propsData:{
+      type: 'setting'
+    }
+  })
+  vm.$mount(div)
+  vm.$on('click',function(){
+    expect(1).to.equal(1)
+  })
+  let button = vm.$el
+  button.click()  
+}
