3fd267ac8fa6f2e8937657bcfc8d257d3ffd1822
xlk <1148038404@qq.com>
2021-01-29 16:01:25 +0800

    完成kkInput 组件

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import kkInput from './kkInput'
 import chai from 'chai'
 import spies from 'chai-spies';
 chai.use(spies);
@@ -11,6 +12,7 @@ var expect = chai.expect
 Vue.component('kk-button', Button)
 Vue.component('kk-icon', Icon)
 Vue.component('kk-button-group', ButtonGroup)
+Vue.component('kk-input', kkInput)
 new Vue({
   el: '#app',
   data:{
@@ -18,7 +20,7 @@ new Vue({
     loading2: false,
     loading3: false,
     loading4: false,
-    
+    msg: ''
   }
 })
 
---
+++ src/kkInput.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="wrapper" :class="{error}">
+    <input :type="type" :value="value" :disabled="disabled" :readonly="readonly" @input="$emit('input',$event.target.value)" @change="$emit('change',$event.target.value)" @focus="$emit('focus',$event.target.value)" @blur="$emit('blur',$event.target.value)">
+    <template v-if="error">
+      <kk-icon type="error" class="iconError"></kk-icon>
+      <span class="errorMessage">{{error}}</span>
+    </template>
+  </div>
+</template>
+
+<script>
+import kkIcon from './icon'
+export default {
+  components: {
+    kkIcon
+  },
+  props: {
+    type: {
+      type: String,
+      default: 'text'
+    },
+    value: {},
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: String
+    }
+
+  },
+  data () {
+    return {
+
+    };
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style scoped lang='scss'>
+$height: 32px;
+$border-color: #999;
+$border-color-hover: #666;
+$border-radius: 4px;
+$font-size: 12px;
+$box-shadow-color: rgba(0, 0, 0, 0.5);
+$red: #f1453d;
+.wrapper {
+  font-size: $font-size;
+  display: inline-flex;
+  align-items: center;
+  vertical-align: middle;
+  > :not(:last-child) {
+    margin-right: 0.5em;
+  }
+  input {
+    height: 32px;
+    width: 128px;
+    font-size: inherit;
+    padding-left: 8px;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    &:hover {
+      border-color: $border-color-hover;
+    }
+    &:focus {
+      box-shadow: inset 0 1px 3px $box-shadow-color;
+      outline: none;
+    }
+    &:active {
+      box-shadow: inset 10 10px 30px $box-shadow-color;
+      outline: none;
+    }
+    &[disabled],
+    &[readonly] {
+      border-color: #bbb;
+      color: #bbb;
+      cursor: not-allowed;
+    }
+  }
+  &.error {
+    > input {
+      border-color: $red;
+    }
+    > .iconError {
+      fill: $red;
+    }
+    > .errorMessage {
+      color: $red;
+    }
+  }
+}
+</style>
